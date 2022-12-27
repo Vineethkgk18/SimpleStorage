dc37a09008eb45a47f780f0ea2ac4dbf109f2c61
Vineeth krishnan <Vineethkgk18@gmail.com>
2022-12-27 16:37:57 +0530

    wallet created

--- deploy.js
+++ deploy.js
@@ -7,6 +7,14 @@ const fs = require("fs-extra");
 async function main(){
 
     const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545");
+    const wallet = new ethers.Wallet(
+        "b5967021650d028c642d5499b3169396ee809292f14e01df6c29eef698097e78",
+        provider
+    ); 
+    const abi = fs.readFileSync(
+        "./SimpleStorage_sol_SimpleStorage.abi", 
+        "utf8"
+    );
     
 }
 
