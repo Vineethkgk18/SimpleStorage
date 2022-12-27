f7f1f93838b25053fd87e7b04c4ddad34b9d93fb
Vineeth krishnan <Vineethkgk18@gmail.com>
2022-12-27 16:36:44 +0530

    ether js included and solidity file compiled

--- deploy.js
+++ deploy.js
@@ -1,1 +1,20 @@
-6.52.57 minutes seconds'
+//  6.52.57 minutes seconds'
+// ganache RPC URL
+// http://127.0.0.1:8545
+
+const ethers = require("ethers");
+const fs = require("fs-extra");
+async function main(){
+
+    const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545");
+    
+}
+
+
+main()
+    .then(()=> process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
+
